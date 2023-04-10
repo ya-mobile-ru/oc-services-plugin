1992d4a437d48c299da20cb1f378858349c6ffd1
NiTika1509 <sicnax@gmail.com>
2023-04-10 15:38:42 +0300

    feat(updates): create migration subservices table

---
+++ updates/create_yamobile_services_subservices.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Yamobile\Services\Updates;
+
+use October\Rain\Database\Updates\Migration;
+use Schema;
+
+class CreateYamobileServicesSubservice extends Migration
+{
+    public function up()
+    {
+        Schema::create('yamobile_services_subservice', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->string('name')->nullable();
+            $table->text('content')->nullable();
+            $table->string('image_url')->nullable();
+            $table->integer('category_id')->primory();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('yamobile_services_subservice');
+    }
+}
