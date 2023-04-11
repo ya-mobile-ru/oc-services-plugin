a46b609588cfcd0602456c762b3987c6d17ae8a8
NiTika1509 <sicnax@gmail.com>
2023-04-11 14:32:12 +0300

    refactor: the navigation menu is placed in a separate file

--- Plugin.php
+++ Plugin.php
@@ -17,30 +17,6 @@ class Plugin extends PluginBase
         ];
     }
 
-    public function registerNavigation()
-    {
-        return [
-            'Services-Menu' => [
-                'label' => 'yamobile.services::lang.details.name',
-                'url' => Backend::url('yamobile/services/services'),
-                'icon' => 'icon-folder',
-                'sideMenu' => [
-                    'Side-Menu-Services'=> [
-                        'label' => 'Услуги',
-                        'url' => Backend::url('yamobile/services/services'),
-                        'icon' => 'icon-sitemap'
-                    ],
-                    'Side-Menu-Subservices' => [
-                        'label' => 'Подуслуги',
-                        'url' => Backend::url('yamobile/services/subservices'),
-                        'icon' => 'icon-list'
-                    ]
-
-                ]
-            ],
-        ];
-    }
-
     public function registerComponents()
     {
     }
