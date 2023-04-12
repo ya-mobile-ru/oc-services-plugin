8f1a0281f88b6c218ccc7338be2a6dad8089f271
NiTika1509 <sicnax@gmail.com>
2023-04-12 11:06:50 +0300

    feat(models): a link to the Subservices model has been created (Many to Once)

--- models/Service.php
+++ models/Service.php
@@ -9,5 +9,8 @@ class Service extends Model
 
     public $table = 'yamobile_services_services';
 
+    public $hasMany = [
+        'subservices' => \Yamobile\Services\Models\Subservice::class
+    ];
 
 }
