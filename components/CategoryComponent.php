99f639e61125a25d606bd64824d4e91e90e45082
NiTika1509 <sicnax@gmail.com>
2023-04-18 17:52:54 +0300

    feat(components): added a check for the presence of a category and the output of 404 error

--- components/CategoryComponent.php
+++ components/CategoryComponent.php
@@ -2,6 +2,8 @@
 
 namespace Yamobile\Services\Components;
 
+
+use Response;
 use Cms\Classes\ComponentBase;
 use Yamobile\Services\Models\Category;
 
@@ -9,6 +11,8 @@ use Yamobile\Services\Models\Category;
 
 class CategoryComponent extends ComponentBase
 {
+
+    public $category;
 
     public function componentDetails()
     {
@@ -35,6 +39,18 @@ class CategoryComponent extends ComponentBase
     public function onRun()
     {
 
+        $this->category = $this->loadCategory();
+
+        if(!$this->category){
+            $this->setStatusCode(404);
+            return $this->controller->run('404');
+        }
+
+    }
+
+
+    private function loadCategory()
+    {
         $slug = $this->property('slug');
 
         $category = Category::where('slug',$slug)
