b509566329dabfe22e57c923e89ba5098fb9d171
NiTika1509 <sicnax@gmail.com>
2023-04-19 09:29:51 +0300

    refactor(models): the class path is placed in the use parameter

--- models/Service.php
+++ models/Service.php
@@ -4,21 +4,18 @@
 namespace Yamobile\Services\Models;
 
 use Model;
+use Yamobile\Services\Models\Category;
 
-/**
- * Model
- */
+
 class Service extends Model
 {
 
-    /**
-     * @var string The database table used by the model.
-     */
+
+
     public $table = 'yamobile_services_services';
 
 
     public $belongsTo = [
-        'category' => \Yamobile\Services\Models\Category::class,
+        'category' => Category::class,
     ];
-
 }
