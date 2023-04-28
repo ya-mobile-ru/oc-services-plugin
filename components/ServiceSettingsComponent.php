508dda5b9b19285845d58f8372fc3b8cc4b78cd8
NiTika1509 <sicnax@gmail.com>
2023-04-28 17:34:39 +0300

    feat(components): created component for output services setting to twig

---
+++ components/ServiceSettingsComponent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Yamobile\Services\Components;
+
+use Cms\Classes\ComponentBase;
+use Yamobile\Locations\Models\ServiceSettings;
+
+class ServiceSettingsComponent extends ComponentBase
+{
+    public $settings;
+
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Настройки услуг',
+            'description' => 'Описание настроек услуг'
+        ];
+    }
+
+    public function onRun()
+    {
+        $this->settings = $this->getServicesSettings();
+    }
+
+    public function getServicesSettings()
+    {
+        return [
+            'image' => ServiceSettings::get('image'),
+            'content' => ServiceSettings::get('content'),
+            'seo_name' => ServiceSettings::get('seo_name'),
+            'meta_title' => ServiceSettings::get('meta_title'),
+            'meta_keywords' => ServiceSettings::get('meta_keywords'),
+            'meta_description' => ServiceSettings::get('meta_description'),
+        ];
+    }
+}
