3a57465e3485cc421e72261906ed4c0f123eef30
NiTika1509 <sicnax@gmail.com>
2023-06-13 11:33:42 +0300

    feat(models): added synchronization of multiple connections

--- models/PriceImport.php
+++ models/PriceImport.php
@@ -26,38 +26,27 @@ class PriceImport extends ImportModel
 
                 if(array_key_exists('id', $data)){
                     $price = Price::find(array_get($data, 'id') );
-                    $dataPrice = [];
-                    foreach ($data as $attribute => $value) {
-                        $dataPrice[] = [
-                            $attribute => $value
-                        ];
+                    if($price){
+                        $price->update(array_except($data, ['id','categories']));
+                        $this->logUpdated();
+                    }else{
+                        $price = Price::create($data);
+                        $this->logCreated();
                     }
-                    $price->update($dataPrice);
                 } else {
                     $price = Price::create($data);
+                    $this->logCreated();
                 }
 
 
                 $category_data = (!empty(array_get($data, 'categories'))) ? array_get($data, 'categories') : false;
 
-                $categoryModel =  null;
 
                 if($category_data){
-                    $categoryModel = Category::where('name', $category_data)->first();
-                    if(!$categoryModel){
-                        $categoryModel = Category::create(['name' => $category_data]);
-                    }
 
-                    $price->categories()->attach($categoryModel->id, ['price_id' => $price->id]);
+                    $price->categories()->sync(self::separateRelationString($category_data));
+
                 }
-
-
-                if (array_key_exists('id', $data)) {
-                    $this->logUpdated();
-                } else {
-                    $this->logCreated();
-                }
-
             }
             catch (\Exception $ex) {
                 $this->logError($row, $ex);
@@ -65,5 +54,33 @@ class PriceImport extends ImportModel
 
         }
     }
-}
 
+
+    static private function separateRelationString($str_category): array
+    {
+
+        $separate = ';';
+
+        $categoryModel =  null;
+
+        $id_category = array();
+
+        foreach (explode($separate,$str_category) as $category_name){
+
+            if(empty($category_name)) continue;
+
+            $categoryModel = Category::where('name', $category_name)->first();
+
+            if(!$categoryModel){
+                $categoryModel = Category::create(['name' => $category_name,'slug' => str_slug($category_name, '-')]);
+            }
+
+            $id_category[] = $categoryModel->id;
+
+        }
+
+        return $id_category;
+
+    }
+
+}
